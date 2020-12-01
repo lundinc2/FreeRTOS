9e0f6469a2e4506434b068fe86465380f97a4107
Carl Lundin <lundinc@amazon.com>
2020-12-01 15:32:22 -0800

    Setup pvPortMalloc proof.

---
+++ FreeRTOS/Test/CBMC2/proofs/pvPortMalloc/pvPortMalloc_harness.c
@@ -0,0 +1,25 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+ * Insert copyright notice
+ */
+
+/**
+ * @file pvPortMalloc_harness.c
+ * @brief Implements the proof harness for pvPortMalloc function.
+ */
+
+/*
+ * Insert project header files that
+ *   - include the declaration of the function
+ *   - include the types needed to declare function arguments
+ */
+
+void harness()
+{
+
+  /* Insert argument declarations */
+
+  pvPortMalloc( /* Insert arguments */ );
+}
